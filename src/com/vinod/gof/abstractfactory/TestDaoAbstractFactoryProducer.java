63f9585b9d83abc2e0b72d3edde3473a8a996222
Vinod Kumar Nair <vinod827@gmail.com>
2019-03-18 07:01:05 +0530

    code updated

--- src/com/vinod/gof/abstractfactory/TestDaoAbstractFactoryProducer.java
+++ src/com/vinod/gof/abstractfactory/TestDaoAbstractFactoryProducer.java
@@ -1,4 +1,8 @@
-package com.vinod.gof.com.vinod.gof.abstractfactory;
+package com.vinod.gof.abstractfactory;
+
+import com.vinod.gof.com.vinod.gof.abstractfactory.Dao;
+import com.vinod.gof.com.vinod.gof.abstractfactory.DaoAbstractFactory;
+import com.vinod.gof.com.vinod.gof.abstractfactory.DaoAbstractFactoryProducer;
 
 public class TestDaoAbstractFactoryProducer {
     public static void main(String[] args) {
